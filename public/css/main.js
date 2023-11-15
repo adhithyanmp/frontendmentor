c8f026a76c990883e5d63947be58a51a2c37cb4a
adhithyanmp <105354910+adhithyanmp@users.noreply.github.com>
2023-11-15 20:54:12 +0530

    Add files via upload

---
+++ public/css/main.js
@@ -0,0 +1,35 @@
+function animo() {
+    var reveals = document.querySelectorAll(".animo");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", animo);
+
+  function abouo() {
+    var reveals = document.querySelectorAll(".abouo");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", abouo);
